6bc66d4b39c60249ba8f36fe0162a854a647aa58
Hester Knight <hester2knight@mac.com>
2020-09-29 22:24:51 +0100

    added newsfeed container as a screen

--- App.js
+++ App.js
@@ -4,20 +4,27 @@ import HeaderBar from "./components/HeaderBar";
 import MessageBar from "./components/MessageBar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import NewsFeedContainer from "./components/NewsfeedContainer";
 
+
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator()
 
 export default function App() {
   return (
-    <NewsFeedContainer />
+   
     <NavigationContainer>
+      {/* // For each drawer we create different screen */}
+      <Drawer.Navigator>
+        <Drawer.Screen name="NewsfeedContainer" component={NewsFeedContainer}/>
+      </Drawer.Navigator>
+
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
         }}
       >
-        {/* // header bar renders first */}
         <Stack.Screen name="HeaderBar" component={HeaderBar} />
         <Stack.Screen name="MessageBar" component={MessageBar} />
       </Stack.Navigator>
