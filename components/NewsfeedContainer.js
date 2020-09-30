30db7859ed92e598acdb1a13ffabe0e63addb65c
Hester Knight <hester2knight@mac.com>
2020-09-30 18:01:43 +0100

    rendered list of hardcoded data to represent posts

--- components/NewsfeedContainer.js
+++ components/NewsfeedContainer.js
@@ -2,12 +2,16 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import HeaderBar from "./HeaderBar";
 import Post from "./Post";
+import { users } from "../test-data/users";
 
 export default function NewsFeedContainer({ navigation }) {
   return (
     <View style={styles.container}>
       <HeaderBar navigation={navigation} />
-      <Post />
+
+      {users.map((user, i) => {
+          return <Post user={user}/>;
+      })}
     </View>
   );
 }
--- components/Post.js
+++ components/Post.js
@@ -2,15 +2,13 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import { Card } from "react-native-elements";
 
-export default function Post() {
+export default function Post({ user }) {
   return (
     <View>
       <Card style={styles.card}>
-        <Card.Title> Post </Card.Title>
+        <Card.Title> {user.name} </Card.Title>
         <Card.Divider />
-        <Card.FeaturedSubtitle> hi </Card.FeaturedSubtitle>
       </Card>
-     
     </View>
   );
 }
