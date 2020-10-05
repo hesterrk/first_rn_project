9bcff94d7daf5842aba1d9773c52babbb08d57f3
Hester Knight <hester2knight@mac.com>
2020-10-05 12:59:51 +0100

    added other post icons with container around all

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, View, Image, Dimensions } from "react-native";
 import { Card } from "react-native-elements";
-import { SimpleLineIcons } from "@expo/vector-icons";
+import { SimpleLineIcons, FontAwesome } from "@expo/vector-icons";
 
 const deviceWidth = Dimensions.get("window").width;
 
@@ -15,7 +15,19 @@ export default function Post({ user }) {
           <Image style={styles.image} source={require("../mockPic.jpg")} />
         </View>
         <View style={styles.iconContainer}>
-          <SimpleLineIcons name="heart" size={24} color="black" />
+          <SimpleLineIcons
+            name="heart"
+            size={24}
+            color="black"
+            style={styles.icon}
+          />
+          <FontAwesome
+            name="comment-o"
+            size={24}
+            color="black"
+            style={styles.icon}
+          />
+          <SimpleLineIcons name="paper-plane" size={24} color="black" />
         </View>
       </Card>
     </View>
@@ -39,5 +51,11 @@ const styles = StyleSheet.create({
 
   iconContainer: {
     paddingTop: 10,
+    backgroundColor: "lavender",
+    display: "flex",
+    flexDirection: "row",
+  },
+  icon: {
+    marginRight: 15,
   },
 });
