72fc94c5ebfa25cbaed062be511bc5221779b1a9
Hester Knight <hester2knight@mac.com>
2020-10-05 13:09:33 +0100

    added containers to accomodate styling of left and right icons

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, View, Image, Dimensions } from "react-native";
 import { Card } from "react-native-elements";
-import { SimpleLineIcons, FontAwesome } from "@expo/vector-icons";
+import { SimpleLineIcons, FontAwesome, Feather } from "@expo/vector-icons";
 
 const deviceWidth = Dimensions.get("window").width;
 
@@ -15,19 +15,24 @@ export default function Post({ user }) {
           <Image style={styles.image} source={require("../mockPic.jpg")} />
         </View>
         <View style={styles.iconContainer}>
-          <SimpleLineIcons
-            name="heart"
-            size={24}
-            color="black"
-            style={styles.icon}
-          />
-          <FontAwesome
-            name="comment-o"
-            size={24}
-            color="black"
-            style={styles.icon}
-          />
-          <SimpleLineIcons name="paper-plane" size={24} color="black" />
+          <View style={styles.leftIconContainer}>
+            <SimpleLineIcons
+              name="heart"
+              size={24}
+              color="black"
+              style={styles.icon}
+            />
+            <FontAwesome
+              name="comment-o"
+              size={24}
+              color="black"
+              style={styles.icon}
+            />
+            <SimpleLineIcons name="paper-plane" size={24} color="black" />
+          </View>
+          <View>
+            <Feather name="bookmark" size={24} color="black" />
+          </View>
         </View>
       </Card>
     </View>
@@ -48,10 +53,15 @@ const styles = StyleSheet.create({
     height: deviceWidth * 0.8,
     width: deviceWidth * 0.8,
   },
-
   iconContainer: {
+    display: "flex",
+    flexDirection: "row",
     paddingTop: 10,
-    backgroundColor: "lavender",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  leftIconContainer: {
     display: "flex",
     flexDirection: "row",
   },
