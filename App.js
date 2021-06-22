1c4f70de300f4e8225156b5c923bc6ca086897b7
Hester <hester2knight@mac.com>
2021-06-22 17:17:53 +0100

    App - updated navigation, nesting stack navigation under tab navigation to account for tab navigation

--- App.js
+++ App.js
@@ -1,25 +1,33 @@
 import "react-native-gesture-handler";
 import React from "react";
-import MessageBar from "./components/MessageBar";
 import MessageContainer from "./components/MessageContainer"
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import NewsFeedContainer from "./components/NewsfeedContainer";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ExplorePageContainer from "./components/ExplorePageContainer";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomePageStackScreen() {
+  return (
+    <Stack.Navigator screenOptions={{
+      headerShown: false
+    }}>
+      <Stack.Screen name="NewsFeedContainer" component={NewsFeedContainer} />
+        <Stack.Screen name="MessageContainer" component={MessageContainer} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="NewsFeedContainer" component={NewsFeedContainer} />
-        {/* <Stack.Screen name="MessageBar" component={MessageBar} /> */}
-        <Stack.Screen name="MessageContainer" component={MessageContainer} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomePageStackScreen} />
+        <Tab.Screen name="Explore" component={ExplorePageContainer} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
